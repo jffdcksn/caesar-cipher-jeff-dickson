000f2aff90330d93dd1d878205bdd243204b615f
Your Name <you@example.com>
2024-03-28 18:18:44 -0400

    first commit

--- cipher.py
+++ cipher.py
@@ -1,1 +1,18 @@
-# add your code here
+text = input("Please enter a sentence: ")
+shift = 5
+
+def caesar(message, offset):
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    encrypted_text = ''
+
+    for char in message.lower():
+        if char == ' ':
+            encrypted_text += char
+        else:
+            index = alphabet.find(char)
+            new_index = (index + offset) % len(alphabet)
+            encrypted_text += alphabet[new_index]
+    print('Please enter a sentence:', message)
+    print('The encrypted sentence is:', encrypted_text)
+
+caesar(text, shift)
